656b04f9d95d6de2ec35aad53070337d606b37a3
GraziCarassini <duegra@gmail.com>
2025-05-19 10:59:50 -0400

    funcionando tudo

--- index.php
+++ index.php
@@ -5,22 +5,23 @@
     <title>Cadastro</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
         <h1>Formulário</h1>
         <p>Preencha os campos abaixo:</p>
 
-        <form action="processa.php" method="post">
-            <label for="nome">Nome:</label>
+        <form action="filtros.php" method="post">
+            <label for="nome">Nome: </label>
             <input type="text" name="nome" id="nome" required>
 
-            <label for="telefone">Telefone:</label>
+            <label for="telefone">Telefone: </label>
             <input type="text" name="telefone" id="telefone" required>
 
-            <label for="email">E-mail:</label>
+            <label for="email">E-mail: </label>
             <input type="text" name="email" id="email" required>
 
-            <label for="senha">Senha:</label>
+            <label for="senha">Senha: </label>
             <input type="password" name="senha" id="senha" required>
 
             <button type="submit">Salvar</button>
